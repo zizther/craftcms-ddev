43e47e23cf12a303bf04df2df69d38246fc539dc
nathan <zizther@gmail.com>
2022-09-07 23:24:44 +0100

    Add env conditional

--- config/vite.php
+++ config/vite.php
@@ -3,7 +3,7 @@
 use craft\helpers\App;
 
 return [
-	'checkDevServer' => true,
+	'checkDevServer' => App::env('CRAFT_ENVIRONMENT') === 'dev',
 	'devServerInternal' => 'http://localhost:3000',
 	'devServerPublic' => Craft::getAlias('@web') . ':3000',
 	'errorEntry' => 'src/js/app.js',
