c0b6e871696567f380009cd0bf33260e9f6cbe81
nathan <zizther@gmail.com>
2022-09-21 17:39:37 +0300

    Vite config update

--- vite.config.js
+++ vite.config.js
@@ -6,44 +6,45 @@ import ViteRestart from 'vite-plugin-restart';
 
 // https://vitejs.dev/config/
 export default defineConfig(({command}) => ({
-  base: command === 'serve' ? '' : '/dist/',
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    base: command === 'serve' ? '' : '/dist/',
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+        manifest: true,
+        outDir: path.resolve(__dirname, 'web/dist/'),
+        rollupOptions: {
+        input: {
+            platform: path.resolve(__dirname, 'src/js/platform.js'),
+            app: path.resolve(__dirname, 'src/js/app.js'),
+        },
+        output: {
+            sourcemap: true
+        },
+        },
     },
-    manifest: true,
-    outDir: path.resolve(__dirname, 'web/dist/'),
-    rollupOptions: {
-      input: {
-        app: path.resolve(__dirname, 'src/js/app.js'),
-      },
-      output: {
-        sourcemap: true
-      },
+    plugins: [
+        manifestSRI(),
+        viteCompression({
+            filter: /\.(js|mjs|json|css|map)$/i
+        }),
+        ViteRestart({
+            reload: [
+                'templates/**/*',
+            ],
+        }),
+    ],
+    publicDir: path.resolve(__dirname, 'src/public'),
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@css': path.resolve(__dirname, 'src/pcss'),
+            '@js': path.resolve(__dirname, 'src/js'),
+        },
     },
-  },
-  plugins: [
-    manifestSRI(),
-    viteCompression({
-      filter: /\.(js|mjs|json|css|map)$/i
-    }),
-    ViteRestart({
-      reload: [
-        'templates/**/*',
-      ],
-    }),
-  ],
-  publicDir: path.resolve(__dirname, 'src/public'),
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@css': path.resolve(__dirname, 'src/css'),
-      '@js': path.resolve(__dirname, 'src/js'),
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        strictPort: true,
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-  },
 }));
