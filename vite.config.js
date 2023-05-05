24b0d803320ad855b0a881a17ed1d3410b474e94
nathan <zizther@gmail.com>
2023-05-05 20:09:30 +0100

    Vite updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import {defineConfig} from 'vite';
+import copy from 'rollup-plugin-copy';
 import manifestSRI from 'vite-plugin-manifest-sri';
 import path from 'path';
+import terser from '@rollup/plugin-terser';
 import viteCompression from 'vite-plugin-compression';
 import ViteRestart from 'vite-plugin-restart';
+import removeConsole from 'vite-plugin-remove-console';
 
 // https://vitejs.dev/config/
 export default defineConfig(({command}) => ({
@@ -14,16 +17,37 @@ export default defineConfig(({command}) => ({
         manifest: true,
         outDir: path.resolve(__dirname, 'web/dist/'),
         rollupOptions: {
-        input: {
-            platform: path.resolve(__dirname, 'src/js/platform.js'),
-            app: path.resolve(__dirname, 'src/js/app.js'),
-        },
-        output: {
-            sourcemap: true
-        },
+            input: {
+                platform: path.resolve(__dirname, 'src/js/platform.js'),
+                app: path.resolve(__dirname, 'src/js/app.js'),
+            },
+            output: {
+                sourcemap: true
+            },
+            plugins: [
+                // Used to remove comments from JS files
+                terser({
+                    format: {
+                        // Remove all comments
+                        comments: false
+                    },
+                    // Prevent any compression
+                    compress: false
+                }),
+                removeConsole()
+            ],
         },
     },
     plugins: [
+        copy({
+            targets: [
+                {
+                    src: 'web/assets/graphics/**/*',
+                    dest: 'web/dist/graphics'
+                }
+            ],
+            hook: 'writeBundle'
+        }),
         manifestSRI(),
         viteCompression({
             filter: /\.(js|mjs|json|css|map)$/i
@@ -43,7 +67,11 @@ export default defineConfig(({command}) => ({
         },
     },
     server: {
+        fs: {
+            strict: false
+        },
         host: '0.0.0.0',
+        origin: 'http://localhost:3000',
         port: 3000,
         strictPort: true,
     },
